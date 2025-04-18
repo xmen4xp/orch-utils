c0608cdce69c3a6698653f9889a08c35fab88cc6
Charles Chan <425857+charlesmcchan@users.noreply.github.com>
2025-04-18 11:14:01 -0700

    feat: allow setting registry/repo name via DOCKER_REGISTRY/DOCKER_REPOSITORY env var (#79)
    Co-authored-by: Zack Williams <zdw@users.noreply.github.com>

--- mage/Magefile.go
+++ mage/Magefile.go
@@ -7,6 +7,7 @@ package mage
 
 import (
 	"fmt"
+	"os"
 	"regexp"
 
 	"github.com/bitfield/script"
@@ -16,10 +17,20 @@ import (
 var PublicCharts = []string{"aws-sm-get-rs-token", "aws-sm-proxy", "oci-secret", "secret-wait", "token-refresh"}
 var PublicContainers = []string{"aws-sm-proxy"}
 
-const (
-	AWSRegion                         = "us-west-2"
-	OpenEdgePlatformRegistryRepoURL   = "080137407410.dkr.ecr.us-west-2.amazonaws.com"
-	OpenEdgePlatformRepository        = "edge-orch"
+var (
+	AWSRegion                       = "us-west-2"
+	OpenEdgePlatformRegistryRepoURL = func() string {
+		if r := os.Getenv("DOCKER_REGISTRY"); r != "" {
+			return r
+		}
+		return "080137407410.dkr.ecr.us-west-2.amazonaws.com"
+	}()
+	OpenEdgePlatformRepository = func() string {
+		if r := os.Getenv("DOCKER_REPOSITORY"); r != "" {
+			return r
+		}
+		return "edge-orch"
+	}()
 	RegistryRepoSubProj               = "common"
 	OpenEdgePlatformContainerRegistry = OpenEdgePlatformRegistryRepoURL + "/" +
 		OpenEdgePlatformRepository + "/" +
